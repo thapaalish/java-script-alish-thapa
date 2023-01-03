72a90fcd55f7c5eedf22730093dbf4cb8f789115
thapaalish <thapaalish44@gmail.com>
2023-01-03 18:59:14 +0545

    hhh

---
+++ alish.js
@@ -0,0 +1,26 @@
+
+
+//1.function definition 
+//function name is always written in camelCase
+function myFunctionA(n){
+    //function body
+    console.log('hello '+n+' everyone');
+}
+//function calling
+myFunctionA('anish dollar');
+
+
+let myFunctionC = (name)=>{
+    console.log('hello '+name+' How are you?')
+
+}
+myFunctionC("alish ");
+
+
+
+let a = ['anil','alish','allos'];
+console.log(a);
+
+let b = [...a,'anihhhhl','alggggish','alddddlos']
+
+console.log(b);
